a166af264517a1122eafaa3cc577c7dfbdbad38a
Soumyadeepta04 <soumyadeeptamanna@gmail.com>
2025-10-03 00:26:22 +0530

    fix deployment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -40,9 +40,9 @@ export async function signup(req,res){
             res.cookie("jwt", token,{
                 maxAge:7*24*60*60*1000,
                 httpOnly: true,
-                sameSite : "strict",
+                sameSite : "none",
                 // secure: process.env.NODE_ENV === "production",
-                secure: false,
+                secure: true,
             })
 
             res.status(201).json({success: true, user:newUser})
