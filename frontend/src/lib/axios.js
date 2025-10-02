195fa6483d6677c844b871d81cd2116ea96b14f3
Soumyadeepta04 <soumyadeeptamanna@gmail.com>
2025-10-03 00:14:13 +0530

    updated backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://talkify-jg35suygb-soumyadeepta-mannas-projects.vercel.app/api",
+  baseURL: "https://talkify-wheat.vercel.app/api",
   withCredentials: true,
 });
 
