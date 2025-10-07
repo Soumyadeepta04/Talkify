7fca7950e3f1511fda3313363c622b730c2b882f
Soumyadeepta04 <soumyadeeptamanna@gmail.com>
2025-10-07 20:03:04 +0530

    fix navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           {/* User profile - This now correctly navigates to /profile */}
           <div className="avatar cursor-pointer" onClick={() => navigate("/profile")}>
             <div className="w-10 h-10 rounded-full overflow-hidden ring-2 ring-primary ring-offset-base-200 ring-offset-2 transition-transform duration-300 transform hover:scale-110">
-              <img src={authUser?.profilePic || "/default-avatar.png"} alt="User Avatar" />
+              <img src={authUser?.profilepic || "/default-avatar.png"} alt="User Avatar" />
             </div>
           </div>
 
